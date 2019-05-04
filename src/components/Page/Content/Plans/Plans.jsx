c066604dd9847946fa72239dc143b6e488ac0125
Sergei <epcijer@gmail.com>
2019-05-04 17:13:48 +0400

    Plans block refactoring

--- src/components/Page/Content/Plans/Plans.jsx
+++ src/components/Page/Content/Plans/Plans.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import radioImage from './img/radio.png'
-import radioCheckmarkImage from './img/radio-mark.png'
-import {visibleHidden} from "../../../../styles/helpers";
+import RadioButton from "../../RadioButton/RadioButton";
 
 
 
@@ -26,63 +24,33 @@ const PlansBlock = styled.div`
     margin-top: 15px;
 `
 
-const Plan = styled.div`
-    display: flex;
-    align-items: center;
-    margin-right: 11px;
-    &:last-child {
-        margin-right: 0;
-    }
-`
-const PlansRadio = styled.input`
-    ${visibleHidden}
-    
-    &:checked + label::before,
-    &:not(:checked) + label::before {
-        content: '';
-        position: absolute;
-        left: 0;
-        top: -3px;
-        width: 37px;
-        height: 25px;
-        border: none;
-        background: url(${radioImage});
-        background-position: center;
-        background-size: cover;
-    }
-    &:checked + label::after,
-    &:not(:checked) + label::after {
-        content: '';
-        position: absolute;
-        width: 21px;
-        height: 20px;
-        background: url(${radioCheckmarkImage});
-        background-position: center;
-        background-size: cover;
-        top: 3px;
-        left: 8px;
-        transition: all 0.2s ease;
-    }
-    &:not(:checked) + label::after {
-        opacity: 0;
-        transform: scale(0);
-    }
-    &:checked + label::after {
-        opacity: 1;
-        transform: scale(1);
-    }
-`
-const PlansLabel = styled.label`
-    position: relative;
-    padding-left: 36px;
-    font-size: 18px;
-    font-weight: bold;
-    cursor: pointer;
-    line-height: 20px;
-    display: inline-block;
-    color: #000;
-`
 
+const plansData = [
+    {
+        content: 'Верстать',
+        active: false,
+        id: 'plan_1',
+        name: 'plan_radio_name'
+    },
+    {
+        content: 'Прокачиваться в JS',
+        active: true,
+        id: 'plan_2',
+        name: 'plan_radio_name'
+    },
+    {
+        content: 'Менеджерство',
+        active: false,
+        id: 'plan_3',
+        name: 'plan_radio_name'
+    },
+    {
+        content: 'Другое',
+        active: false,
+        id: 'plan_4',
+        name: 'plan_radio_name'
+    }
+]
 
 const Plans = () => {
     return (
@@ -90,22 +58,9 @@ const Plans = () => {
             <PlansTitle>Какие у вас планы на будущее?</PlansTitle>
             <PlansText>Этот ответ ни&nbsp;на&nbsp;что не&nbsp;повлияет. Не&nbsp;беда, если в&nbsp;будущем ваши планы поменяются.</PlansText>
             <PlansBlock>
-                <Plan>
-                    <PlansRadio type="radio" id="plan1" name="plan"/>
-                    <PlansLabel for="plan1">Верстать</PlansLabel>
-                </Plan>
-                <Plan>
-                    <PlansRadio type="radio" id="plan2" name="plan" defaultChecked/>
-                    <PlansLabel for="plan2">Прокачиваться в JS</PlansLabel>
-                </Plan>
-                <Plan>
-                    <PlansRadio type="radio" id="plan3" name="plan"/>
-                    <PlansLabel for="plan3">Менеджерство</PlansLabel>
-                </Plan>
-                <Plan>
-                    <PlansRadio type="radio" id="plan4" name="plan"/>
-                    <PlansLabel for="plan4">Другое</PlansLabel>
-                </Plan>
+                {plansData.map(({content, name, id, active}) =>
+                    <RadioButton title="plan" name={name} id={id} defaultChecked={active}>{content}</RadioButton>
+                )}
             </PlansBlock>
         </PlansWrap>
     )
---
+++ src/components/Page/RadioButton/RadioButton.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import styled from 'styled-components'
+import {visibleHidden} from "../../../styles/helpers";
+import radioImage from "../Content/Plans/img/radio.png";
+import radioCheckmarkImage from "../Content/Plans/img/radio-mark.png";
+
+
+const RadioButtonWrap = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 11px;
+    &:last-child {
+        margin-right: 0;
+    }
+`
+const PlansLabel = styled.label`
+    position: relative;
+    padding-left: 36px;
+    font-size: 18px;
+    font-weight: bold;
+    cursor: pointer;
+    line-height: 20px;
+    display: inline-block;
+    color: #000;
+    &::before {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: -3px;
+        width: 37px;
+        height: 25px;
+        border: none;
+        background: url(${radioImage});
+        background-position: center;
+        background-size: cover;
+    }
+    &::after {
+        content: '';
+        position: absolute;
+        width: 21px;
+        height: 20px;
+        background: url(${radioCheckmarkImage});
+        background-position: center;
+        background-size: cover;
+        top: 3px;
+        left: 8px;
+        transition: all 0.2s ease;
+    }
+`
+const PlansRadio = styled.input`
+    ${visibleHidden}
+
+    &:not(:checked) + ${PlansLabel}::after {
+        opacity: 0;
+        transform: scale(0);
+    }
+    &:checked + ${PlansLabel}::after {
+        opacity: 1;
+        transform: scale(1);
+    }
+`
+
+
+const RadioButton = ({name, id, children, defaultChecked}) => {
+    return (
+        <RadioButtonWrap>
+            <PlansRadio name={name} id={id} type="radio" defaultChecked={defaultChecked}/>
+            <PlansLabel for={id}>{children}</PlansLabel>
+        </RadioButtonWrap>
+
+    )
+}
+
+export default RadioButton;
