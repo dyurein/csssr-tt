202d16439d963dbe050603cfe0bfd6843bec475d
Sergei <epcijer@gmail.com>
2019-05-04 13:05:50 +0400

    Checkbox  fix

--- src/components/Page/Checkbox/Checkbox.jsx
+++ src/components/Page/Checkbox/Checkbox.jsx
@@ -1,33 +1,53 @@
 import React from 'react'
-import styled, {css} from 'styled-components'
+import styled from 'styled-components'
 import checkbox from "../../../images/checkbox.png";
-import checkbox_checked from '../../../images/checked.png'
+import checked from '../../../images/checked.png'
 
 
 const CheckboxWrap = styled.div`
     margin-bottom: 17px;
 `
 const CheckboxLabel = styled.label`
-    font-size: 21px;
+    font-size: 18px;
     font-weight: bold;
-    line-height: 25px;
+    line-height: 20px;
     color: #000;
     user-select: none;
     margin-bottom: 9px;
     position: relative;
-    padding-left: 25px;
-    &:last-child{
+    padding-left: 26px;
+    cursor: pointer;
+    display: inline-block;
+    &:last-child {
         margin-bottom: 0;
         width: 200px;
     }
-
-    ${props => props.active && css`{
-        border: 1px solid red
-        &::before {
-            background-color: #000;
-        }
-    `}
+    &::before {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 18px;
+      height: 18px;
+      border: none;
+      background: url(${checkbox});
+      background-position: center;
+      background-size: cover;
+    }
+    &::after {
+      content: '';
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      background: url(${checked});
+      background-position: center;
+      background-size: cover;
+      top: -6px;
+      left: 1px;
+      transition: all 0.2s ease;
+    }
 `
+
 const CheckboxInput = styled.input`
     position:absolute;
     left:-10000px;
@@ -35,44 +55,7 @@ const CheckboxInput = styled.input`
     width:1px;
     height:1px;
     overflow:hidden;
-    
-    &:checked + label,
-    &:not(:checked) + label {
-        position: relative;
-        padding-left: 26px;
-        font-size: 18px;
-        font-weight: bold;
-        cursor: pointer;
-        line-height: 20px;
-        display: inline-block;
-        color: #000;
-    }
-    &:checked + label::before,
-    &:not(:checked) + label::before {
-        content: '';
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 18px;
-        height: 18px;
-        border: none;
-        background: url(${checkbox});
-        background-position: center;
-        background-size: cover;
-    }
-    &:checked + label::after,
-    &:not(:checked) + label::after {
-        content: '';
-        position: absolute;
-        width: 20px;
-        height: 20px;
-        background: url(${checkbox_checked});
-        background-position: center;
-        background-size: cover;
-        top: -6px;
-        left: 1px;
-        transition: all 0.2s ease;
-    }
+
     &:not(:checked) + label::after {
         opacity: 0;
         transform: scale(0);
--- src/components/Page/Content/AboutSkills/AboutSkills.jsx
+++ src/components/Page/Content/AboutSkills/AboutSkills.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import styled, {css} from 'styled-components'
-
+import styled from 'styled-components'
 import Checkbox from "../../Checkbox/Checkbox";
+
+
 
 
 const AboutSkillsWrap = styled.div`
