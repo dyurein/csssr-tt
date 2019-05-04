7164b639b85d15c25206057309eac1d256740729
Sergei <epcijer@gmail.com>
2019-05-04 13:26:23 +0400

    styles for visibleHidden

---
+++ src/styles/helpers.js
@@ -0,0 +1,11 @@
+import {css} from "styled-components";
+
+
+export const visibleHidden = css`
+    position:absolute;
+    left:-10000px;
+    top:auto;
+    width:1px;
+    height:1px;
+    overflow:hidden;
+`
